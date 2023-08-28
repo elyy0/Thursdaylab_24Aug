273847572af377133bfaee4c8b697a0ce8c470fd
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-28 12:27:06 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package question3;
+public class main
+{
+	public static void main(String[] args)
+	{
+		//Creating the object of Surgeon class and passing the values in parameter for parameterized constructor
+        Surgeon surgeon=new Surgeon("Alice","Surgeon","Neuro");
+        //Creating the object of Nurse class and passing the values in parameter for parameterized constructor
+        Nurse nurse=new Nurse("Bob","Intern","Ortho","Orthopedics");
+        //calling the methods
+        surgeon.introduce();
+        surgeon.performSurgery();
+        nurse.introduce();
+        nurse.assistInSurgery();
+    }
+}
